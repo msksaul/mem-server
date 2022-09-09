df221efac4ab458d382846edb550425303053136
mscalel <msksaul@gmail.com>
2022-09-09 17:17:03 -0600

    Like Post, env variables

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('<h1>Hello</h1>')
 })
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://jsmastery:jsmastery123@cluster0.xdmuffq.mongodb.net/posts-db?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
